aac30767475b038656394d2b890e04600da594f4
MarinaEstefania <m.garciac11@gmail.com>
2019-08-04 15:32:53 -0500

    Updates

---
+++ precache-manifest.5574ca9b5625ebb742c1165f75e4773d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b29691a2ecc2d0f657f72fbcd62bba",
+    "url": "/melp/index.html"
+  },
+  {
+    "revision": "9e99a90653b859dd65e6",
+    "url": "/melp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/melp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9e99a90653b859dd65e6",
+    "url": "/melp/static/js/main.974eda56.chunk.js"
+  },
+  {
+    "revision": "d39c81b98576d920f40f",
+    "url": "/melp/static/js/runtime~main.9d090ed1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/melp/static/media/logo.5d5d9eef.svg"
+  }
+]);
