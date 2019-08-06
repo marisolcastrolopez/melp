a93761f6bd81d2b49db248f228595641e06acb0b
marisolcastrolopez <solcastrodg@gmail.com>
2019-08-06 10:06:10 -0500

    Login stylized

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from "mdbreact";
+import '../style/login.css'
 
 const Login = () => {
   return (
     <MDBContainer>
       <MDBRow>
         <MDBCol md="6">
-          <form>
+          <form className="form-padding">
             <p className="h5 text-center mb-4">Sign in</p>
             <div className="grey-text">
               <MDBInput
                 label="Type your email"
-                icon="envelope"
+                // icon="envelope"
                 group
                 type="email"
                 validate
@@ -21,17 +22,16 @@ const Login = () => {
               />
               <MDBInput
                 label="Type your password"
-                icon="lock"
+                // icon="lock"
                 group
                 type="password"
                 validate
               />
             </div>
-            <div className="text-center">
-              <MDBBtn>
-                
-              </MDBBtn>
-              <Link style={{ textDecoration: "none", color: "white" }} to="/app">About</Link>
+            <div className="text-center btn-login">
+              <Link className="link-login" to="/app">
+                Login
+              </Link>
             </div>
           </form>
         </MDBCol>
